df6e359da3f01d6c669bca5401a9b023023b6ff9
choss <clayton.hoss@googlemail.com>
2019-03-08 16:08:12 +0100

    Added script for symlinking secrets

---
+++ scripts/symlink_files.sh
@@ -0,0 +1,5 @@
+ln -s /config/secrets/hassio_secrets.yaml /config/secrets.yaml
+ln -s /config/secrets/esphome_secrets.yaml /config/esphome/secrets.yaml
+
+ln -s /config/secrets/ssl/fullchain.pem /ssl/fullchain.pem
+ln -s /config/secrets/ssl/privkey.pem /ssl/privkey.pem
