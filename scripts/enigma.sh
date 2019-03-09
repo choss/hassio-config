1ae9b7efac6d23efa888daa1274c8ebf191768e7
choss <clayton.hoss@googlemail.com>
2019-03-09 14:55:38 +0100

    Changed paths in CI config and trying to add scripts for build

--- scripts/enigma.sh
+++ scripts/enigma.sh
@@ -2,7 +2,7 @@
 rm -rf homeassistant-enigma-player
 # git clone https://github.com/cinzas/homeassistant-enigma-player
 git clone https://github.com/sechsbug/homeassistant-enigma-player.git
-mkdir -p /config/custom_components
+mkdir -p ../custom_components
 rm -f homeassistant-enigma-player/README.md
 rm -rf homeassistant-enigma-player/screenshots
-cp -r homeassistant-enigma-player/* /config/custom_components/
+cp -r homeassistant-enigma-player/* ../custom_components/
--- scripts/tk.sh
+++ scripts/tk.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 rm -rf homeassistant-tankerkoenig
 git clone https://github.com/panbachi/homeassistant-tankerkoenig.git
-mkdir -p /config/custom_components
-cp -r homeassistant-tankerkoenig/custom_components/* /config/custom_components/
-
+mkdir -p ../custom_components
+cp -r homeassistant-tankerkoenig/custom_components/* ../custom_components/
