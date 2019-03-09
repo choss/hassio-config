540023fde3c0b2e277944b09b2d502d421af098b
choss <clayton.hoss@googlemail.com>
2019-03-09 17:17:48 +0100

    Downloading custom components via wget

--- scripts/enigma.sh
+++ scripts/enigma.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 rm -rf homeassistant-enigma-player
 # git clone https://github.com/cinzas/homeassistant-enigma-player
-git clone https://github.com/sechsbug/homeassistant-enigma-player.git
+# git clone https://github.com/sechsbug/homeassistant-enigma-player.git
+wget https://github.com/cinzas/homeassistant-enigma-player/archive/v1.1.zip -O enigma.zip
+unzip enigma.zip
+rm -f enigma.zip
+mv homeassistant-enigma-player* homeassistant-enigma-player
 mkdir -p ../custom_components
 rm -f homeassistant-enigma-player/README.md
 rm -rf homeassistant-enigma-player/screenshots
+rm -f homeassistant-enigma-player/custom_components.json
 cp -r homeassistant-enigma-player/* ../custom_components/
--- scripts/tk.sh
+++ scripts/tk.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 rm -rf homeassistant-tankerkoenig
-git clone https://github.com/panbachi/homeassistant-tankerkoenig.git
+#git clone https://github.com/panbachi/homeassistant-tankerkoenig.git
+wget https://github.com/panbachi/homeassistant-tankerkoenig/archive/0.2.0.zip -O tk.zip
+unzip tk.zip
+rm -f tk.zip
+mv homeassistant-tankerkoenig* homeassistant-tankerkoenig
 mkdir -p ../custom_components
 cp -r homeassistant-tankerkoenig/custom_components/* ../custom_components/
