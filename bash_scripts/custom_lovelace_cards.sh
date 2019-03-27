879114350af76a2ec2dc9969d87537e1ac31dc07
choss <clayton.hoss@googlemail.com>
2019-03-27 11:21:05 +0100

    adding script for fetching custom lovelace cards

---
+++ bash_scripts/custom_lovelace_cards.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir -p ../www
+
+# mini media player
+wget https://github.com/kalkih/mini-media-player/releases/download/v1.0.3/mini-media-player-bundle.js
+mv mini-media-player-bundle.js ../www/
