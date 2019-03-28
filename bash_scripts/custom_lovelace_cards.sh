63a69aa77a7481e41a3627ba3d85cde1b0a380a4
choss <clayton.hoss@googlemail.com>
2019-03-28 20:10:37 +0100

    adding lovelace slider

--- bash_scripts/custom_lovelace_cards.sh
+++ bash_scripts/custom_lovelace_cards.sh
@@ -4,3 +4,7 @@ mkdir -p ../www
 # mini media player
 wget https://github.com/kalkih/mini-media-player/releases/download/v1.0.3/mini-media-player-bundle.js
 mv mini-media-player-bundle.js ../www/
+
+#lovelace-entity-slider
+git clone https://github.com/thomasloven/lovelace-slider-entity-row.git
+mv lovelace-slider-entity-row/slider-entity-row.js ../www/
