4dd5d67529811b87ca474a85409de3afae5d65b9
choss <clayton.hoss@googlemail.com>
2019-03-30 19:27:39 +0100

    prefering wget over git

--- bash_scripts/custom_lovelace_cards.sh
+++ bash_scripts/custom_lovelace_cards.sh
@@ -6,5 +6,5 @@ wget https://github.com/kalkih/mini-media-player/releases/download/v1.0.3/mini-m
 mv mini-media-player-bundle.js ../www/
 
 #lovelace-entity-slider
-git clone https://github.com/thomasloven/lovelace-slider-entity-row.git
-mv lovelace-slider-entity-row/slider-entity-row.js ../www/
+wget https://raw.githubusercontent.com/thomasloven/lovelace-slider-entity-row/master/slider-entity-row.js
+mv slider-entity-row.js ../www/
