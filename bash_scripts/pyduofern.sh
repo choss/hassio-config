b1a727100b4171ba46214804ce63d759342bce28
choss <clayton.hoss@googlemail.com>
2019-04-23 10:35:18 +0200

    build: updating script for duofern

--- bash_scripts/pyduofern.sh
+++ bash_scripts/pyduofern.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 rm -rf pyduofern
 
-git clone https://github.com/gluap/pyduofern/
+wget https://github.com/gluap/pyduofern/archive/master.zip
+unzip master.zip
+rm -f master.zip
+mv pyduofern* pyduofern
 
 mkdir -p ../custom_components
 cp -r pyduofern/examples/homeassistant/custom_components/duofern ../custom_components/
