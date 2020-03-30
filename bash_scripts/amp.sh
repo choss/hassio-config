b5393b5a566269b588e45f72bc86d7dcc70a4a55
choss <clayton.hoss@googlemail.com>
2020-03-30 13:51:11 +0200

    hass: renaming entities/fixing alexa

--- bash_scripts/amp.sh
+++ bash_scripts/amp.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 rm -rf alexa_media_player
-wget https://github.com/keatontaylor/alexa_media_player/archive/v1.3.1.zip -O amp.zip
+wget https://github.com/custom-components/alexa_media_player/releases/download/v2.5.6/alexa_media.zip -O amp.zip
 
-unzip amp.zip
+unzip amp.zip -d alexa_media
 rm -f amp.zip
-mv alexa_media_player* alexa_media_player
 mkdir -p ../custom_components
-cp -r alexa_media_player/custom_components/alexa_media ../custom_components/
+cp -r alexa_media ../custom_components/
