db206e36634f8f6ddadc2599f594a851e0a52208
choss <clayton.hoss@googlemail.com>
2020-12-13 16:57:58 +0100

    hass: updates

--- bash_scripts/amp.sh
+++ bash_scripts/amp.sh
@@ -2,7 +2,7 @@
 rm -rf alexa_media
 mkdir -p alexa_media
 
-wget https://github.com/custom-components/alexa_media_player/releases/download/v2.5.6/alexa_media.zip -O amp.zip
+wget https://github.com/custom-components/alexa_media_player/releases/download/v3.2.3/alexa_media.zip -O amp.zip
 
 unzip amp.zip -d alexa_media
 rm -f amp.zip
