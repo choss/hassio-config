033634dd589a2c028dd6ebaf2359145657183487
choss <clayton.hoss@googlemail.com>
2021-03-01 07:50:14 +0100

    Update duofern.sh
    changing location of duofern component

--- bash_scripts/duofern.sh
+++ bash_scripts/duofern.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 rm -rf pyduofern
 
-wget https://github.com/gluap/pyduofern/archive/master.zip
+wget https://github.com/gluap/pyduofern-hacs/archive/master.zip
+
 unzip master.zip
 rm -f master.zip
-mv pyduofern* pyduofern
+mv pyduofern-hacs* pyduofern-hacs
 
 mkdir -p ../custom_components
-cp -r pyduofern/examples/homeassistant/custom_components/duofern ../custom_components/
+cp -r pyduofern-hacs/custom_components/duofern ../custom_components/
